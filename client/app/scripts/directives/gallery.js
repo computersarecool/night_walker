e33c5198c65bda5a47df027ee5fb7dddb6541a39
computersarecool <willynolan1988@gmail.com>
2015-01-27 19:43:46 -0800

    Functioning phone rotation gallery

--- client/app/scripts/directives/gallery.js
+++ client/app/scripts/directives/gallery.js
@@ -10,7 +10,7 @@
 angular.module('nightwalkerApp')
   .directive('siteGallery', function ($location, $interval, $timeout, $window) {
   
-    var grayPant = angular.element('<div id="bottom-gallery" class="pant-sprite gray-pant gallery-animate">Some More words here</div>');
+    var grayPant = angular.element('<div id="bottom-gallery" class="pant-sprite gray-pant">Some More words here</div>');
     var colorPant = angular.element('<div id="top-gallery" class="pant-sprite blue-pant">Some words here</div>');
     var list = angular.element('<p></p>');
 
@@ -18,6 +18,7 @@ angular.module('nightwalkerApp')
       var index = 0;
       var classes = ['blue-pant', 'red-pant'];
       colorPant.addClass(classes[index])
+      grayPant.addClass('gallery-animate');
 
       return function () {
         grayPant.addClass('displayed');
@@ -36,40 +37,77 @@ angular.module('nightwalkerApp')
     var manualChange = (function () {
       // Listen for the deviceorientation event and handle the raw data
       $window.addEventListener('deviceorientation', function(eventData) {
+
+        grayPant.addClass('gallery-fast');
         // gamma is the left-to-right tilt in degrees, where right is positive
         var tiltLR = eventData.gamma;
         // beta is the front-to-back tilt in degrees, where front is positive
         var tiltFB = eventData.beta;
         // alpha is the compass direction the device is facing in degrees
         var dir = eventData.alpha
+        console.log(tiltLR);
 
-        switch (tiltLR) {
+        switch (true) {
           case tiltLR < -40:
             //BLUE
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('blue-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR >= -37 && tiltLR < -29:
             //PURPLE
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('red-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR >= -26 && tiltLR < -18:
             //PINK
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('blue-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR >= -15 && tiltLR < -7:
             //POWDER PINK
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('red-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR >= -4 && tiltLR  < 4:
             //RED
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('blue-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR >= 7 && tiltLR < 15:
             //ORANGE
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('red-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR >= 18 && tiltLR < 26:
             //ORANGE
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('blue-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR >= 29 && tiltLR  < 37:
             //YELLOW
+            colorPant.removeAttr('class');
+            colorPant.addClass('pant-sprite');
+            colorPant.addClass('blue-pant');
+            grayPant.removeClass('displayed');
             break;
           case tiltLR > 40:
              //GREEN
+             grayPant.removeClass('displayed');
+            grayPant.removeClass('displayed');
             break;
 
           case tiltLR >= -40 && tiltLR < -37:
@@ -80,6 +118,8 @@ angular.module('nightwalkerApp')
           case tiltLR >= 15 && tiltLR < 18:
           case tiltLR >= 26 && tiltLR < 29:
           case tiltLR >= 37 && tiltLR < 40:
+            grayPant.addClass('displayed');
+            console.log('something');
             //GRAY
             break
         } 
