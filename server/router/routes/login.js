ab329f15f7d0345b30d34906ac819382d8ea5de6
computersarecool <willynolan1988@gmail.com>
2015-02-02 22:11:04 -0800

    First working example with passport

--- server/router/routes/login.js
+++ server/router/routes/login.js
@@ -2,77 +2,22 @@ var express = require('express');
 var jwt = require('jsonwebtoken');
 var expressJwt = require('express-jwt');
 var db = require('../../database');
-
+var passport = require('passport');
 var Users = db.users;
 var jwtSecret = require('../../../config/credentials').jwtSecret;
 
+require('../../inner_config/passport')(passport);
 
 var router = express.Router();
 
-
-
-function authenticate (req, res, next) {
-  var body = req.body;
-
-
-  if (!body.username || !body.password) {
-    res.status(400).end('Must provide username or password'); 
-    return
-  }
-
-
-  // Actually need to check username and password
-  Users.findOne({
-    'username': body.username
-  }, function (err, user) {
-
-    if (err) {
-      //Log a message so you can see what happened
-      console.log("Couldn't verify user because of: " + err);
-      
-      res.status(500).json({
-        'Message': 'Internal server error from logging in new user. Please contact support@help.com'
-      });
-    }
-
-    if (!user) {
-      
-      console.log('THAT USER IS NOT HERE');
-      res.status(401).end('Invalid user');
-
-    }    
-
-    if (user) {
-      console.log('Yes there is a user');
-
-      user.checkPassword (body.password, function (err, auth) {
-        if (err) {
-          throw err
-        }
-
-        if (!auth) {
-          res.status(401).end('Wrong password'); 
-          return
-        }
-
-        if (auth) {
-          req.user = user;
-          next();
-        }
-
-      });
-    }
-
-
-  });
-}
-
-
-
-router.post('/login', authenticate, function (req, res) {
+router.post('/login', passport.authenticate('local-login', {session: false}), function (req, res) {
+  
+  console.log('Here we go');
+  console.log(req.user);
 
   var token = jwt.sign({
-    //This is where the jot is created
+    //This is where the jwt is created
+    funThing: 'This is your personal JWT',
     username: req.user.username
   }, jwtSecret);
 
--- server/router/routes/me.js
+++ server/router/routes/me.js
@@ -5,7 +5,8 @@ var expressJwt = require('express-jwt');
 var jwtSecret = require('../../../config/credentials').jwtSecret;
 
 router.use('/', expressJwt({secret: jwtSecret}), function (err, req, res, next) {
-  //This sets req.user with the decoded JWT.(i.e. the JWT)
+  // This sets req.user with the decoded JWT.(i.e. the JWT)
+  // To be used later to authenticate user
   if (err) {
     console.log('An error happened');
     throw err;
@@ -14,10 +15,20 @@ router.use('/', expressJwt({secret: jwtSecret}), function (err, req, res, next) 
 });
 
 router.get('/', function (req, res) {
-  console.log(req.user);
+  if (req.user.username === 'johnDoe') {
+    
+    console.log('John Doe is here');
+
+    res.send({
+      "name": 'Hi there john'
+    })
+
+  }
+
   res.send({
     "name": "Congrats"
   })
+
 });
 
 module.exports = router;
