9581010378388d597cc9d77186b55be58da70be4
computersarecool <willynolan1988@gmail.com>
2015-02-03 22:47:50 -0800

    Improved database model

--- server/database/schemas/users.js
+++ server/database/schemas/users.js
@@ -2,6 +2,14 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 var Schema = mongoose.Schema;
 
+//Neccesary schemas to make:
+  // -orders
+  // -Items
+  // -coupon
+  // -help request
+  // -Credit card arrays
+
+
 //User Schema
 var userSchema = new Schema({
 
@@ -10,6 +18,39 @@ var userSchema = new Schema({
     // required: true,
     // unique: true
   },
+  emailAddress: {
+    // Make an array
+    type: String
+  },
+  streetAddress: {
+    // Make an array
+    type: String
+  },
+  coupons: {
+    // Make an array
+    type: Array
+  },
+  orders: {
+    type: Array
+    // Add Sub Type
+  },
+  firstName: String,
+  lastName: String,
+  middleName: String,
+  creditCarts: {
+    type: [creditCards]
+  },
+  paymentAccounts: [paymentAccounts],
+  cart: {
+    type: Array
+  },
+  helpRequests: {
+    type: Array
+  }
+  created: {
+    type: Date,
+    default: Date.now
+  },   
   password: {
     type: String
     // required: true
@@ -33,18 +74,17 @@ userSchema.pre('save', function (next) {
 });
 */
 
-//Password verification helper
-/*userSchema.method.comparePassword = function (triedPassword, callback) {
+
+userSchema.methods.checkPassword = function (triedPassword, callback) {
+  /*
   bcrypt.compare(triedPassword, this.password, function (err, isMatch) {
     if (err) {
       return callback(err)
     }
     callback(null, isMatch);
   });
-};
-*/
+  */
 
-userSchema.methods.checkPassword = function (triedPassword, callback) {
   // Right password
   if (triedPassword === this.password) {
     console.log('The password is right');
