03b3be11b302709cdb05d6598ffe6afdb92ff0fe
computersarecool <willynolan1988@gmail.com>
2015-02-03 23:48:49 -0800

    Removed unneeded comments

--- server/database/schemas/users.js
+++ server/database/schemas/users.js
@@ -15,24 +15,18 @@ var userSchema = new Schema({
 
   username: {
     type: String
-    // required: true,
-    // unique: true
   },
   emailAddress: {
-    // Make an array
     type: String
   },
   streetAddress: {
-    // Make an array
     type: String
   },
   coupons: {
-    // Make an array
     type: Array
   },
   orders: {
     type: Array
-    // Add Sub Type
   },
   firstName: String,
   lastName: String,
@@ -53,37 +47,15 @@ var userSchema = new Schema({
   },   
   password: {
     type: String
-    // required: true
   } 
+  
 });
 
 
-/*
-//A method that is called every time a user document is saved...
-userSchema.pre('save', function (next) { 
-  var user = this;
-    //If the password hasn't been modified continue
-    if (!user.isModified('password')) {
-      return next();
-    }
-
-    bcrypt.hash(user.password, 10, function (err, hash) {
-      user.password = hash;
-      next();
-    });
-});
-*/
-
+//Encrypt before save userSchema.pre('save')
 
 userSchema.methods.checkPassword = function (triedPassword, callback) {
-  /*
-  bcrypt.compare(triedPassword, this.password, function (err, isMatch) {
-    if (err) {
-      return callback(err)
-    }
-    callback(null, isMatch);
-  });
-  */
+  //bcrypt.compare
 
   // Right password
   if (triedPassword === this.password) {
