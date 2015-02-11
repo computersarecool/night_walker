40301a0ee71d0d4fdd24cf3e238fe2b2d5390c40
computersarecool <willynolan1988@gmail.com>
2015-02-11 00:45:39 -0800

    Find unique colors with a distinct operator in mongoose

--- server/router/routes/allproducts.js
+++ server/router/routes/allproducts.js
@@ -4,17 +4,11 @@ var moment = require('moment');
 var db = require('../../database');
 var Products = db.Products;
 router.get('/', function (req, res) {
-  Products.aggregate([{
-    $group: {
-      _id: '$color'
-    }
-  }], {}, function (err, collection) {
-    console.log(err, collection);
+  Products.collection.distinct('color', function (err, collection) {
     if (err) {
       throw err
     }
-    res.end();
-    // res.json(collection);
+    console.log(collection);
   });
 });
 
