97ab1cfa8c8d96654e1fc3407262f3231892e057
computersarecool <willynolan1988@gmail.com>
2015-02-23 03:02:32 -0800

    Add regex to check for underscore in req.param

--- server/router/routes/getproduct.js
+++ server/router/routes/getproduct.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Products = require('../../../database').Products;
 
 router.get('/:flavor', function (req, res) {
-  var flavor = req.params.flavor;
+  var flavor = req.params.flavor.replace(/_/g, ' ');;
   Products.findOne({flavor:flavor}, function (err, product) {
     if (err) {
       throw err
