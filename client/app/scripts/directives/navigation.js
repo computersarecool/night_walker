ffc58706f13999d467d57c853aafa0146a5490ea
computersarecool <willy@willynolan.com>
2015-11-13 05:38:10 +0000

    Adds animator factory and nav list animation

--- client/app/scripts/directives/navigation.js
+++ client/app/scripts/directives/navigation.js
@@ -7,7 +7,7 @@
  * # navigation
  */
 angular.module('nightwalkerApp')
-  .directive('siteNav', function ($location) {
+  .directive('siteNav', function ($location, $timeout) {
     return {
       restrict: 'E',
       templateUrl: 'partials/navigation.html',
@@ -15,7 +15,11 @@ angular.module('nightwalkerApp')
         scope.toggleNav = function (e) {
           if (e.target !== e.currentTarget) {
             if ($location.path() === '/') {
-              e.currentTarget.className = 'horizontal';
+              e.currentTarget.className = 'collapsed';
+              var thing = e.currentTarget;
+              $timeout(function (navUl) {
+                navUl.className = 'horizontal';
+              }, 2000, true, e.currentTarget);
             }
           }
           // TODO: Figure out why e.stopPropgation here causes problems
