98bbdb43d19ae6c49fbbea985cdb76957d885911
computersarecool <willy@willynolan.com>
2016-02-11 00:21:18 +0000

    Adds cart route

---
+++ client/app/scripts/controllers/cart.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nightwalkerApp.controller:CartCtrl
+ * @description
+ * # CartCtrl
+ * Controller of the nightwalkerApp
+ */
+angular.module('nightwalkerApp')
+  .controller('CartCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
