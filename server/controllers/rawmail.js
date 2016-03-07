37a1963f63926473610d97cad74a15ab6d5d26d7
computersarecool <willy@willynolan.com>
2016-03-07 05:03:06 +0000

    Creates separate controllers for simple and raw mail

---
+++ server/controllers/rawmail.js
@@ -0,0 +1,114 @@
+var http = require('http');
+var async = require('async');
+var aws = require('aws-sdk');
+var fileType = require('file-type');
+var accessKeyId = require('../../../../../safe/credentials').aws_access_key_id;
+var secretAccessKey = require('../../../../../safe/credentials').aws_secret_access_key;
+var region = 'us-west-2';
+
+aws.config.update({accessKeyId: accessKeyId, secretAccessKey: secretAccessKey, region: region});
+var ses = new aws.SES();
+
+// Takes a url, filename and function. Downloads, returns mime and binary data base64 encoded
+function getInfo (fileData, callback) {
+  http.get(fileData.url, function (res) {
+    var details;
+    var buffer;
+    var chunks = [];
+
+    res.once('data', function (chunk) {
+      details = fileType(chunk);
+    });
+
+    res.on('data', function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on('end', function () {
+      buffer = Buffer.concat(chunks).toString('base64');
+      callback({
+        mimetype: details.mime,
+        filename: fileData.filename + '.' + details.ext,
+        file: buffer,
+      });
+    });
+  });
+}
+
+// fromName, fromEmail, mainTarget, subject, body, files, allRecipients
+function sendEmail (options) {
+  var boundary = 'boundarydivider';    
+  var mimeversion = '1.0';
+  
+  var ses_mail = "From: " + options.fromName + " " + "<" + options.fromEmail + ">" + "\n";  
+  ses_mail += "To: " + options.mainTarget + "\n";
+  ses_mail += "Subject: " + options.subject + "\n";
+  ses_mail += "MIME-Version: " + mimeversion + "\n";
+  ses_mail += "Content-Type: multipart/mixed; boundary=" + boundary + "\n\n";
+  ses_mail += "--" + boundary + "\n";
+  ses_mail += "Content-Type: text/html; charset=us-ascii\n\n";
+  ses_mail += options.body + "\n\n";
+
+  async.each(options.files, function (file, callback) {
+    getInfo(file, function (info) {
+      ses_mail += "--" + boundary + "\n";
+      ses_mail += "Content-Type: " + info.mimetype + ";name= " + info.filename + "\n";
+      ses_mail += "Content-Disposition: attachment; filename=" + info.filename + "\n";
+      ses_mail += "Content-Transfer-Encoding: base64\n\n",
+      ses_mail += info.file + "\n";
+      callback();
+    });
+  }, function (err) {
+      if (err) {
+        throw err;
+      }
+    
+      // Final boundary marker
+      ses_mail += "--" + boundary;
+
+      // Set email parameters
+      var params = {
+        RawMessage: {Data: new Buffer(ses_mail)},
+        Destinations: options.allRecipients,
+        Source: options.fromEmail,
+      };
+
+      // Actually send the email
+      ses.sendRawEmail(params, function(err, data) {
+        if (err) {
+          console.log('there was an error', err);
+        } 
+        else {
+          console.log(data);
+        }           
+      });
+    });
+};
+
+// TEST
+var myOptions = {
+  fromName: '"Willy Nolan"',
+  fromEmail: 'willy@willynolan.com',
+  mainTarget: 'willy@willynolan.com',
+  subject: 'test this out',
+  body: 'this is the body of my email',
+  files: [
+    {
+      filename: 'tester',
+      url: 'http://i.imgur.com/Mkzfi2h.jpg',
+    },
+    {
+      filename: 'test2',
+      url: 'http://i.imgur.com/dbFyEF3.jpg',
+    }
+  ],
+  allRecipients: ['willy@willynolan.com'],
+};
+
+sendEmail(myOptions);
+
+
+module.exports = {
+  sendEmail: sendEmail,
+};
+
---
+++ server/controllers/simplemail.js
@@ -0,0 +1,75 @@
+var fs = require('fs');
+var path = require('path');
+
+var aws = require('aws-sdk');
+
+var accessKeyId = require('../../../../../safe/credentials').aws_access_key_id;
+var secretAccessKey = require('../../../../../safe/credentials').aws_secret_access_key;
+var region = 'us-west-2';
+
+aws.config.update({accessKeyId: accessKeyId, secretAccessKey: secretAccessKey, region: region});
+var ses = new aws.SES();
+
+// TEST:
+var data = {
+  firstName: 'joe',
+  lastName: 'smith',
+  trackingCode: 444,
+  toAddresses: [
+    'willy@willynolan.com',
+    'paperwork@willynolan.com',
+  ],
+  subject: 'testing',
+  fromAddress: 'willy@willynolan.com',
+};
+
+// Pass in firstName, lastName, trackingCode, toAddresses, subject, fromAddress
+function emailCustomer (emailInfo) {
+  var outgoingEmail;
+  
+  var firstNameMatch = /#FIRSTNAME/;
+  var lastNameMatch = /#LASTNAME/;
+  var trackingCodeMatch = /#TRACKINGCODE/;
+
+  var template = fs.readFile(path.join(__dirname, '../templates/emails', 'customer_confirmation.html'), {encoding: 'utf-8'}, function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+      outgoingEmail = data.replace(firstNameMatch, emailInfo.firstName);
+      outgoingEmail = outgoingEmail.replace(lastNameMatch, emailInfo.lastName);
+      outgoingEmail = outgoingEmail.replace(trackingCodeMatch, emailInfo.trackingCode);
+
+      var testParams = {
+        Destination: {
+          ToAddresses: emailInfo.toAddresses,
+        },
+        Message: {
+          Subject: {
+            Data: emailInfo.subject,
+          },
+          Body: {
+            Html: {
+              'Data': outgoingEmail,
+            },
+          },
+        },
+        Source: emailInfo.fromAddress,
+      };
+      
+      ses.sendEmail(testParams, function (err, id) {
+        if (err) {
+          throw err;
+        }
+        console.log('The id is', id);
+      });
+    }
+  });
+}
+
+//TEST
+emailCustomer(data);
+
+module.exports = {
+  emailCustomer: emailCustomer,
+};
+
