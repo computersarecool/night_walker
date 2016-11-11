30d904623041118d691a15b1431658cf2f381e16
computersarecool <willy@willynolan.com>
2016-11-11 20:41:25 +0000

    Adds support for autoprefixing

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -4,31 +4,37 @@ const useref = require('gulp-useref')
 const gulpIf = require('gulp-if')
 const uglify = require('gulp-uglify')
 const stylus = require('gulp-stylus')
+const autoprefixer = require('gulp-autoprefixer')
 const cssnano = require('gulp-cssnano')
 const rename = require('gulp-rename')
 const del = require('del')
-const nib = require('nib')
 const dist = '../server/dist'
 
+// keep dependencies up to date with bower.json
 gulp.task('bower', () => {
   return gulp.src('app/index.html')
     .pipe(wiredep())
     .pipe(gulp.dest('app'))
 })
 
+// update stylus
 gulp.task('stylus', () => {
   del('app/styles/main.css').then(() => gulp.src('app/styles/source.styl')
-    .pipe(stylus({use: [nib()]}))
+    .pipe(stylus())
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions']
+    }))
     .pipe(rename('main.css'))
     .pipe(gulp.dest('app/styles/'))
   )
 })
 
+// delete the already existing dist folder
 gulp.task('clean:dist', () => {
   return del.sync(dist)
 })
 
-// this will go through the <!--build: blocks and concat and update references
+// go through the <!--build: blocks and concat / minify and update references
 gulp.task('useref', () => {
   return gulp.src('app/index.html')
     .pipe(useref())
