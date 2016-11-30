b4bb914f8a33d9a4bcb7ff6a5b3ae403604dfe0d
computersarecool <willy@willynolan.com>
2016-11-30 10:01:34 +0000

    Adds download controller

---
+++ server/controllers/downloader.js
@@ -0,0 +1,34 @@
+const url = require('url')
+const path = require('path')
+const request = require('request')
+// Downloads and names url file, returns content-type and binary data base64 encoded
+
+function downloadFile (fileObj, callback) {
+  console.log('downloading')
+  request.get(fileObj.url).on('response', (res) => {
+    const datachunks = []
+
+    // TODO: Internal error handling
+    res.on('error', (err) => {
+      callback(err)
+    })
+
+    res.on('data', (chunk) => {
+      datachunks.push(chunk)
+    })
+
+    res.on('end', () => {
+      let buffer = Buffer.concat(datachunks).toString('base64')
+      callback(null, {
+        // TODO: Make filename a real path name
+        filename: path.basename(url.parse(fileObj.url).pathname),
+        mimetype: res.headers['content-type'],
+        file: buffer
+      })
+    })
+  })
+}
+
+module.exports = {
+  downloadFile: downloadFile
+}
