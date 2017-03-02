31a17ef454f3f4eb0ffcde43d5c6602a1e16f26b
computersarecool <willy@willynolan.com>
2017-03-02 01:14:51 +0000

    Puts back correct secret for jwt

--- server/router/routes/addproduct.js
+++ server/router/routes/addproduct.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const expressJwt = require('express-jwt')
-const {jwtSecret} = require('../../../credentials')
+const secret = require('../../../credentials').jwtSecret
 const databaseController = require('../../controllers/database')
 
 // Verify the JWT and sets req.user to JWT contents
-router.post('/', expressJwt({jwtSecret}), (req, res, next) => {
+router.post('/', expressJwt({secret}), (req, res, next) => {
   databaseController.findUserAndUpdate(req.user.email, req.body.items, req.body.replace, (err, user) => {
     if (err) {
       return next(err)
